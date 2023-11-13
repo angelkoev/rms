1039cf6a36a86a48ee0d5d8ec6a3e2722a94cac6
Angel Koev <angel_koev@abv.bg>
2023-11-14 01:55:51 +0200

    fixed login

--- src/main/java/com/rms/service/ApplicationUserDetailsService.java
+++ src/main/java/com/rms/service/ApplicationUserDetailsService.java
@@ -9,13 +9,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Service
+
 public class ApplicationUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
