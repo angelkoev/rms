cbb89ab6ad4d7f25857dca71a13dd05eb3173b4a
Angel Koev <angel_koev@abv.bg>
2023-12-03 21:59:00 +0200

    moving maintenance to separate controller

--- src/main/java/com/rms/web/HomeController.java
+++ src/main/java/com/rms/web/HomeController.java
@@ -41,42 +41,42 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/maintenance")
-    public String maintenance(Authentication authentication, RedirectAttributes redirectAttributes) {
-
-        boolean isAdmin = false;
-        for (GrantedAuthority authority : authentication.getAuthorities()) {
-            String role = authority.getAuthority();
-            if ("ROLE_ADMIN".equals(role)) {
-                isAdmin = true;
-            }
-        }
-
-        String infoMessage = "";
-        if (isAdmin && maintenanceInterceptor.isMaintenanceMode()) {
-            infoMessage = "В момента сайта се намира в режим на профилактика!";
-            redirectAttributes.addFlashAttribute("infoMessage", infoMessage);
-            return "redirect:/home";
-        }
-
-
-        if (maintenanceInterceptor.isMaintenanceMode()) {
-            return "maintenance";
-        }
-
-        return "home";
-    }
-
-    @GetMapping("/maintenance/start")
-    public String startMaintenance() {
-        maintenanceInterceptor.activateMaintenanceMode();
-        return "redirect:/";
-    }
-
-    @GetMapping("/maintenance/stop")
-    public String stopMaintenance() {
-        maintenanceInterceptor.deactivateMaintenanceMode();
-        return "redirect:/";
-    }
+//    @GetMapping("/maintenance")
+//    public String maintenance(Authentication authentication, RedirectAttributes redirectAttributes) {
+//
+//        boolean isAdmin = false;
+//        for (GrantedAuthority authority : authentication.getAuthorities()) {
+//            String role = authority.getAuthority();
+//            if ("ROLE_ADMIN".equals(role)) {
+//                isAdmin = true;
+//            }
+//        }
+//
+//        String infoMessage = "";
+//        if (isAdmin && maintenanceInterceptor.isMaintenanceMode()) {
+//            infoMessage = "В момента сайта се намира в режим на профилактика!";
+//            redirectAttributes.addFlashAttribute("infoMessage", infoMessage);
+//            return "redirect:/home";
+//        }
+//
+//
+//        if (maintenanceInterceptor.isMaintenanceMode()) {
+//            return "maintenance";
+//        }
+//
+//        return "home";
+//    }
+//
+//    @GetMapping("/maintenance/start")
+//    public String startMaintenance() {
+//        maintenanceInterceptor.activateMaintenanceMode();
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/maintenance/stop")
+//    public String stopMaintenance() {
+//        maintenanceInterceptor.deactivateMaintenanceMode();
+//        return "redirect:/";
+//    }
 
 }
--- src/main/java/com/rms/web/MaintenanceController.java
+++ src/main/java/com/rms/web/MaintenanceController.java
@@ -1,50 +1,57 @@
-//package com.rms.web;
-//
-//import com.rms.interceptors.MaintenanceInterceptor;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//
-//@RequestMapping("/maintenance")
-//public class MaintenanceController {
-//
-//    private final MaintenanceInterceptor maintenanceInterceptor;
-//
-//    public MaintenanceController(MaintenanceInterceptor maintenanceInterceptor) {
-//        this.maintenanceInterceptor = maintenanceInterceptor;
-//    }
-//
-//    @GetMapping("/")
-//    public String maintenancePage(Authentication authentication, RedirectAttributes redirectAttributes) {
-//        boolean isAdmin = false;
-//        for (GrantedAuthority authority : authentication.getAuthorities()) {
-//            String role = authority.getAuthority();
-//            if ("ROLE_ADMIN".equals(role)) {
-//                isAdmin = true;
-//            }
-//        }
-//
-//        String infoMessage = "";
-//        if (isAdmin) {
-//            infoMessage = "В момента сайта се намира в maintenance режим!";
-//            redirectAttributes.addFlashAttribute("infoMessage", infoMessage);
-//            return "redirect:/home";
-//        }
-//
-//        return "redirect:maintenance";
-//    }
-//
-//    @GetMapping("/start")
-//    public String startMaintenance() {
-//        maintenanceInterceptor.activateMaintenanceMode();
-//        return "redirect:maintenance";
-//    }
-//
-//    @GetMapping("/stop")
-//    public String stopMaintenance() {
-//        maintenanceInterceptor.deactivateMaintenanceMode();
-//        return "redirect:maintenance";
-//    }
-//}
+package com.rms.web;
+
+import com.rms.interceptors.MaintenanceInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/maintenance")
+public class MaintenanceController {
+
+    private final MaintenanceInterceptor maintenanceInterceptor;
+
+    public MaintenanceController(MaintenanceInterceptor maintenanceInterceptor) {
+        this.maintenanceInterceptor = maintenanceInterceptor;
+    }
+
+    @GetMapping("/")
+    public String maintenancePage(Authentication authentication, RedirectAttributes redirectAttributes) {
+        boolean isAdmin = false;
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+            String role = authority.getAuthority();
+            if ("ROLE_ADMIN".equals(role)) {
+                isAdmin = true;
+            }
+        }
+
+        String infoMessage = "";
+        if (isAdmin && maintenanceInterceptor.isMaintenanceMode()) {
+            infoMessage = "В момента сайта се намира в режим на профилактика!";
+            redirectAttributes.addFlashAttribute("infoMessage", infoMessage);
+            return "redirect:/home";
+        }
+
+
+        if (maintenanceInterceptor.isMaintenanceMode()) {
+            return "maintenance";
+        }
+
+        return "home";
+    }
+
+    @GetMapping("/start")
+    public String startMaintenance() {
+        maintenanceInterceptor.activateMaintenanceMode();
+        return "redirect:/";
+    }
+
+    @GetMapping("/stop")
+    public String stopMaintenance() {
+        maintenanceInterceptor.deactivateMaintenanceMode();
+        return "redirect:/";
+    }
+}
