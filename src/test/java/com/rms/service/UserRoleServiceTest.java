e82ae4e2a96229ccd14ca26e6b8927bf62d6193b
Angel Koev <angel_koev@abv.bg>
2023-12-07 11:36:33 +0200

    add tests for UserRoleService

---
+++ src/test/java/com/rms/service/UserRoleServiceTest.java
@@ -0,0 +1,60 @@
+package com.rms.service;
+
+import com.rms.model.entity.UserRoleEntity;
+import com.rms.model.entity.UserRoleEnum;
+import com.rms.repository.UserRoleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+class UserRoleServiceTest {
+
+    @Mock
+    private UserRoleRepository userRoleRepository;
+
+    @InjectMocks
+    private UserRoleService userRoleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindUserRoleEntityByRole_AdminRoleExists() {
+        // Arrange
+        UserRoleEnum adminRole = UserRoleEnum.ADMIN;
+        UserRoleEntity adminUserRoleEntity = new UserRoleEntity();
+        adminUserRoleEntity.setRole(adminRole);
+        adminUserRoleEntity.setDescription("The owner of the company");
+
+        when(userRoleRepository.findUserRoleEntityByRole(adminRole)).thenReturn(adminUserRoleEntity);
+
+        // Act
+        UserRoleEntity result = userRoleService.findUserRoleEntityByRole(adminRole);
+
+        // Assert
+        assertEquals(adminUserRoleEntity, result);
+    }
+
+    @Test
+    void testFindUserRoleEntityByRole_NonExistingRole() {
+        // Arrange
+        UserRoleEnum nonExistingRole = UserRoleEnum.USER;
+
+        when(userRoleRepository.findUserRoleEntityByRole(nonExistingRole)).thenReturn(null);
+
+        // Act
+        UserRoleEntity result = userRoleService.findUserRoleEntityByRole(nonExistingRole);
+
+        // Assert
+        assertNull(result);
+    }
+
+}
